037f85c9e580728b1ebf242b06357a5ed3e81c5a
Alec Snyder <linuxbash8@gmail.com>
2020-02-21 11:00:20 -0800

    Add cur and curv aliases

--- nnn/setup_nnn.zsh
+++ nnn/setup_nnn.zsh
@@ -1,6 +1,5 @@
 export NNN_PLUG=';:vim_open.sh;d:dup.sh;g:git_graph.sh;h:xxd.sh'
 export NNN_OPENER=~/.config/nnn/opener.sh
-export CURRENT_PROJECT=~/Projects/git/github.com/Originate/talon-detector
 export NNN_BMS="b:$CURRENT_PROJECT;c:~/Projects/git/github.com;d:~/Downloads;p:~/Projects;g:~/Projects/git"
 
 alias n='nnn -c -Q'
